c2306ae5b2e91ede1e455b3572d91f08fe53d417
kimjh <clilseoo2@gmail.com>
2024-05-17 16:33:14 +0900

    init

---
+++ src/main/java/com/example/init/InitApplication.java
@@ -0,0 +1,24 @@
+package com.example.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class InitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitApplication.class, args);
+    }
+
+}
+
+@Controller
+class InitController {
+
+    @GetMapping("/")
+    public String init() {
+        return "index";
+    }
+}
