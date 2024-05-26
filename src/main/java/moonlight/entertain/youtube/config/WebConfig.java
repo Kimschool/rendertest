17a608f513e7d89cbe6211c70237dec42dd52958
kimjh <clilseoo2@gmail.com>
2024-05-26 13:36:53 +0900

    aa

---
+++ src/main/java/moonlight/entertain/youtube/config/WebConfig.java
@@ -0,0 +1,19 @@
+package moonlight.entertain.youtube.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "https://revolutionary-tamqrah-weavus-b33365e5.koyeb.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
